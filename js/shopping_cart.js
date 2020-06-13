476d399ab981c557c81c261a43ade6b36e6e46bc
Matt Steinwachs <Matt.Steinwachs@gmail.com>
2020-06-13 14:33:08 -0700

    Added comments and multiple examples of loops and string building to the shopping cart

--- js/shopping_cart.js
+++ js/shopping_cart.js
@@ -19,11 +19,22 @@ var cart = [
 function renderCartItem(item){
   let new_item = document.createElement('div');
   new_item.classList.add("item");
+
+  // Two examples of building a string
+
+  // String concatenation
+  // new_item.innerHTML = (
+  //   '<p>'+
+  //     item.name+" x"+item.quantity+" @ $"+item.price+" each = $"+item.price*item.quantity+
+  //   '</p>'
+  // );
+
+  // String Interpolation
   new_item.innerHTML = (
-    '<p>'+
-      item.name+" x"+item.quantity+" @ $"+item.price+" each = $"+item.price*item.quantity+
-    '</p>'
-  );
+    `<p>${item.name} x${item.quantity} @ $${item.price} each = $${item.price*item.quantity}</p>`
+  )
+
+  //https://medium.com/@endubueze00/javascript-basics-string-concatenation-with-variables-and-interpolation-deba239debbe
 
   let itemList = document.getElementById("items");
   itemList.append(new_item);
@@ -33,35 +44,39 @@ function renderTotal(items){
   let total = 0;
 
   // loops here to calculate total
+
+  let i = 0;
+  while(i < cart.length){
+    total += items[i].price*items[i].quantity
+    i++;
+  }
+
+  // for (let i = 0; i < items.length; i++){
+  //   total += items[i].price*items[i].quantity
+  // }
+
   // items.forEach(item => {
   //   total += item.price * item.quantity
   // });
-
-  for (let i = 0; i < items.length; i++){
-    total += items[i].price*items[i].quantity
-  }
-
-  // let i = 0;
-  // while(i < cart.length){
-  //   rtotal += items[i].price*items[i].quantity
-  //   i++;
-  // }
 
   document.getElementById("total").innerHTML = "$"+total;
 }
 
 // loops here to render each cart item
+// https://computersciencewiki.org/index.php/Loops
+// https://www.w3schools.com/js/js_loop_for.asp
+
+let i = 0;
+while(i < cart.length){
+  renderCartItem(cart[i]);
+  i++;
+}
+
+// for (let i = 0; i < cart.length; i++){
+//   renderCartItem(cart[i]);
+// }
 
 // cart.forEach(renderCartItem);
 
-for (let i = 0; i < cart.length; i++){
-  renderCartItem(cart[i]);
-}
-
-// let i = 0;
-// while(i < cart.length){
-//   renderCartItem(cart[i]);
-//   i++;
-// }
 
 renderTotal(cart);
